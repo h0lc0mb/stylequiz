5e328d41db385449782d674f67e18a58771bbda2
Susan Holcomb <thefurtheroff@gmail.com>
2014-02-11 12:26:31 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stylequiz::Application.config.secret_token = '8a16972516d6cbd6a9e60f65a94d6c93e66f999a55e3b0238094c3a42e32aad3c53db01f27ce188beb4ca145c4ede265c99d9a56c324a97e59095bc87f01688d'
