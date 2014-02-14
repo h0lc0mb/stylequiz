cf40f255ded5e3a62ad0ebf1c448b42b6e1977eb
Susan Holcomb <thefurtheroff@gmail.com>
2014-02-13 18:02:06 -0800

    fixing sql

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,7 @@ class QuestionsController < ApplicationController
   	@test = Test.find(params[:test_id])
   	@question = @test.questions.build(params[:question])
   	if @question.save
-      if Question.where("test_id == ? ", @question.test_id).count<20
+      if Question.where("test_id = ? ", @question.test_id).count<20
   		  redirect_to @test
       else
         redirect_to test_complete_path(id: @test)
